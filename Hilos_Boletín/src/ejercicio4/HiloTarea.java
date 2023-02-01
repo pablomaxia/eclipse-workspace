00f91f804233e122b00c8d25dd23756a7245652f
pablomaxia <pablo.maxia.rodriguez.alu@iesjulioverne.es>
2023-02-01 11:13:47 +0100

    boletin hiloss

--- Hilos_Boletín/src/ejercicio4/HiloTarea.java
+++ Hilos_Boletín/src/ejercicio4/HiloTarea.java
@@ -1,9 +1,7 @@
 package ejercicio4;
 
 public class HiloTarea extends Thread {
-	static HiloTarea hilo1 = new HiloTarea("Thread-0", 2, false);
-	static HiloTarea hilo2 = new HiloTarea("Thread-1", 1, false);
-	
+
 	public HiloTarea(String nombre, int prioridad, boolean demonio) {
 		setName(nombre);
 		setPriority(prioridad);
@@ -21,14 +19,7 @@ public class HiloTarea extends Thread {
 	}
 
 	public synchronized void enviarMensaje(Mensaje m) {
-		Thread t;
-		if (currentThread().getName().equals("Thread-0")) {
-			t = hilo2;
-		}
-		else {
-			t = hilo1;
-		}
-		System.out.println(getName() + " enterado de mensaje de: " + t.getName() + ": " + m.getTexto());
+		System.out.println(getName() + " enterado de mensaje de: " + getName() + ": " + m.getTexto());
 		try {
 			sleep(3000);
 		} catch (InterruptedException e) {
@@ -39,6 +30,7 @@ public class HiloTarea extends Thread {
 	}
 
 	public static void main(String[] args) {
-		hilo1.run();
+		HiloTarea hilo1 = new HiloTarea("Thread-0", 2, false);
+		HiloTarea hilo2 = new HiloTarea("Thread-1", 1, false);
 	}
 }
