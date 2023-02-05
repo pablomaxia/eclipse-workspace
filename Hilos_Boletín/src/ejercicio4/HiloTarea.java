8ae0e819e64b71aff27c193e31b52bf9521d8169
northstarlh <115696735+pablomaxia@users.noreply.github.com>
2023-02-05 20:11:14 +0100

    boletin hilos completo

--- Hilos_Boletín/src/ejercicio4/HiloTarea.java
+++ Hilos_Boletín/src/ejercicio4/HiloTarea.java
@@ -1,25 +1,29 @@
 package ejercicio4;
 
 public class HiloTarea extends Thread {
+	static HiloTarea hilo1, hilo2;
 
 	public HiloTarea(String nombre, int prioridad, boolean demonio) {
 		setName(nombre);
 		setPriority(prioridad);
 		setDaemon(demonio);
-		start();
 	}
 
 	@Override
 	public void run() {
 		for (int i = 0; i <= 10; i++) {
 			Mensaje mensaje = new Mensaje("MSG" + i);
-			enviarMensaje(mensaje);
+			Thread hilo = null; 
+			
+			hilo = currentThread() == hilo1? hilo2: hilo1;
+			enviarMensaje(mensaje,hilo);
 		}
 
 	}
 
-	public synchronized void enviarMensaje(Mensaje m) {
-		System.out.println(getName() + " enterado de mensaje de: " + getName() + ": " + m.getTexto());
+	public synchronized void enviarMensaje(Mensaje m, Thread t) {
+
+		System.out.println(getName() + " enterado de mensaje de: " + t.getName() + ": " + m.getTexto());
 		try {
 			sleep(3000);
 		} catch (InterruptedException e) {
@@ -30,7 +34,17 @@ public class HiloTarea extends Thread {
 	}
 
 	public static void main(String[] args) {
-		HiloTarea hilo1 = new HiloTarea("Thread-0", 2, false);
-		HiloTarea hilo2 = new HiloTarea("Thread-1", 1, false);
+		try {
+			hilo1 = new HiloTarea("Thread-0", 2, false);
+			hilo2 = new HiloTarea("Thread-1", 1, false);
+
+			hilo1.start();
+			hilo2.start();
+
+			hilo1.join();
+			hilo2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
